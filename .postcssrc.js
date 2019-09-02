0104c28450b48fb4da231232f85f090f2b895ba8
wanghxing <1079563056@qq.com>
2019-09-02 18:55:07 +0800

    Update .postcssrc.js

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
@@ -9,15 +8,3 @@ module.exports = {
     "autoprefixer": {}
   }
 }
-=======
-// https://github.com/michael-ciniawsky/postcss-load-config
-
-module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
-    // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
-}
->>>>>>> 61e8362f42aab394c6a1cd54605bb694e3b797e7
