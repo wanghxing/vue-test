1bf02bcb7089b013e4725a37c819ac0e04ef8939
wanghxing <1079563056@qq.com>
2019-09-04 18:47:56 +0800

    dist

--- config/index.js
+++ config/index.js
@@ -42,12 +42,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),   //之前是 '../dist/index.html'
+    index: path.resolve(__dirname, '../docs/index.html'),   //之前是 '../dist/index.html'
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),  // 之前是 '../dist'
+    assetsRoot: path.resolve(__dirname, '../docs'),  // 之前是 '../dist'
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',  // 之前是 '/'
+    assetsPublicPath: './',  // 之前是 '/'
 
     /**
      * Source Maps
